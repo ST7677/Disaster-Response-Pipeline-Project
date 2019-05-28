c3f38922c7498351ca25c8f07481e7c02bbed54a
ST7677 <47414181+ST7677@users.noreply.github.com>
2019-05-28 12:12:45 +0530

    Update run.py

--- app/run.py
+++ app/run.py
@@ -1,6 +1,7 @@
 import json
 import plotly
 import pandas as pd
+import numpy as np
 
 from nltk.stem import WordNetLemmatizer
 from nltk.tokenize import word_tokenize
@@ -25,25 +26,38 @@ def tokenize(text):
 
     return clean_tokens
 
-# load data
+ # load data
 engine = create_engine('sqlite:///../data/DisasterResponse.db')
 df = pd.read_sql_table('CleanedDataTable', engine)
 
-# load model
+ # load model
 model = joblib.load("../models/classifier.pkl")
 
 
-# index webpage displays cool visuals and receives user input text for model
+ # index webpage displays cool visuals and receives user input text for model
 @app.route('/')
 @app.route('/index')
 def index():
-    
-    # extract data needed for visuals
+
+     # extract data needed for visuals
     # TODO: Below is an example - modify to extract data for your own visuals
     genre_counts = df.groupby('genre').count()['message']
     genre_names = list(genre_counts.index)
+
+    # extract data exclude related
+    categories = df.iloc[:,4:]
+    categories_mean = categories.mean().sort_values(ascending=False)[1:11]
+    categories_names = list(categories_mean.index)
+
+    # Show distribution of different category
+    category = list(df.columns[4:])
+    category_counts = []
+    for column_name in category:
+        category_counts.append(np.sum(df[column_name]))
+
     
-    # create visuals
+
+     # create visuals
     # TODO: Below is an example - modify to create your own visuals
     graphs = [
         {
@@ -54,7 +68,7 @@ def index():
                 )
             ],
 
-            'layout': {
+             'layout': {
                 'title': 'Distribution of Message Genres',
                 'yaxis': {
                     'title': "Count"
@@ -73,12 +87,12 @@ def index():
             ],
 
             'layout': {
-                'title': 'Distribution of Message Categories',
+                'title': 'Distribution of different types of Messages',
                 'yaxis': {
                     'title': "Count"
                 },
                 'xaxis': {
-                    'title': "Category"
+                    'title': "Type"
                 }
             }
         },
@@ -91,35 +105,37 @@ def index():
             ],
 
             'layout': {
-                'title': 'Top 10 Message Categories',
+                'title': 'Top 10 types of messages',
                 'yaxis': {
-                    'title': "Percentage"
+                    'title': "Percentages"
                 },
                 'xaxis': {
                     'title': "Categories"
                 }
             }
         }
-    ]    
-    # encode plotly graphs in JSON
+    ]
+    
+
+     # encode plotly graphs in JSON
     ids = ["graph-{}".format(i) for i, _ in enumerate(graphs)]
     graphJSON = json.dumps(graphs, cls=plotly.utils.PlotlyJSONEncoder)
-    
-    # render web page with plotly graphs
+
+     # render web page with plotly graphs
     return render_template('master.html', ids=ids, graphJSON=graphJSON)
 
 
-# web page that handles user query and displays model results
+ # web page that handles user query and displays model results
 @app.route('/go')
 def go():
     # save user input in query
     query = request.args.get('query', '') 
 
-    # use model to predict classification for query
+     # use model to predict classification for query
     classification_labels = model.predict([query])[0]
     classification_results = dict(zip(df.columns[4:], classification_labels))
 
-    # This will render the go.html Please see that file. 
+     # This will render the go.html Please see that file. 
     return render_template(
         'go.html',
         query=query,
